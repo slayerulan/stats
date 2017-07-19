5c48228587a826dcf198de1ef979f650a387fc8c
savik <savik94mail@gmail.com>
2017-07-19 17:59:40 +0300

    some fixes

--- analysis/src/test/java/com/savik/parser/football/bets/AbstractBetTest.java
+++ analysis/src/test/java/com/savik/parser/football/bets/AbstractBetTest.java
@@ -3,13 +3,14 @@ package com.savik.parser.football.bets;
 import javax.transaction.Transactional;
 
 import org.junit.Before;
+import org.junit.runner.RunWith;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SuppressWarnings("AbstractClassWithoutAbstractMethods")
-@Transactional
-@Rollback
+@RunWith(SpringRunner.class)
 public abstract class AbstractBetTest {
-+
+
     @Before
     public void init() {
 
--- analysis/src/test/java/com/savik/parser/football/bets/handicap/PeriodFavoriteHandicapTest.java
+++ analysis/src/test/java/com/savik/parser/football/bets/handicap/PeriodFavoriteHandicapTest.java
@@ -1,9 +1,18 @@
 package com.savik.parser.football.bets.handicap;
 
+import static org.junit.Assert.assertTrue;
+
+import com.savik.parser.football.bets.AbstractBetTest;
+import org.junit.Test;
+
 /**
  * @author Savushkin Yauheni
  * @since 19.07.2017
  */
-public class PeriodFavoriteHandicapTest {
+public class PeriodFavoriteHandicapTest extends AbstractBetTest {
 
+    @Test
+    public void testSuccessfulRemoval() throws Exception {
+        assertTrue(1 == 1);
+    }
 }
