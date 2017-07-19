4d140b53fd581a7f413522c957630f675f4c7693
savik <savik94mail@gmail.com>
2017-07-19 18:21:08 +0300

    some fixes

--- analysis/src/test/java/com/savik/parser/football/bets/AbstractBetTest.java
+++ analysis/src/test/java/com/savik/parser/football/bets/AbstractBetTest.java
@@ -1,19 +1,39 @@
 package com.savik.parser.football.bets;
 
-import javax.transaction.Transactional;
+import java.util.concurrent.ThreadLocalRandom;
 
+import com.savik.football.model.Period;
 import org.junit.Before;
 import org.junit.runner.RunWith;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @SuppressWarnings("AbstractClassWithoutAbstractMethods")
 @RunWith(SpringRunner.class)
 public abstract class AbstractBetTest {
 
+    private static final ThreadLocalRandom RANDOM = ThreadLocalRandom.current();
+
+    private static final Integer MAX_GOALS = 10;
+
+    private static final Integer RANDOM_GOAL_AMOUNT = RANDOM.nextInt(0, MAX_GOALS);
+
     @Before
     public void init() {
 
+    }
+
+    public Period createPeriodHomeWonWithDifference(int diff) {
+        return Period.builder()
+                     .guestScore(RANDOM_GOAL_AMOUNT)
+                     .homeScore(RANDOM_GOAL_AMOUNT + diff)
+                     .build();
+    }
+
+    public Period createPeriodGuestWonWithDifference(int diff) {
+        return Period.builder()
+                     .guestScore(RANDOM_GOAL_AMOUNT + diff)
+                     .homeScore(RANDOM_GOAL_AMOUNT)
+                     .build();
     }
 
 }
--- analysis/src/test/java/com/savik/parser/football/bets/handicap/PeriodFavoriteHandicapTest.java
+++ analysis/src/test/java/com/savik/parser/football/bets/handicap/PeriodFavoriteHandicapTest.java
@@ -2,7 +2,10 @@ package com.savik.parser.football.bets.handicap;
 
 import static org.junit.Assert.assertTrue;
 
+import com.savik.football.bets.handicap.PeriodFavoriteHandicap;
+import com.savik.football.model.Who;
 import com.savik.parser.football.bets.AbstractBetTest;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -11,8 +14,20 @@ import org.junit.Test;
  */
 public class PeriodFavoriteHandicapTest extends AbstractBetTest {
 
+    public static final Double HANDICAP = -1.5;
+
+    private PeriodFavoriteHandicap periodFavoriteHandicap;
+
+    @Before
+    public void createRule() {
+        periodFavoriteHandicap = new PeriodFavoriteHandicap(HANDICAP);
+    }
+
     @Test
-    public void testSuccessfulRemoval() throws Exception {
-        assertTrue(1 == 1);
+    public void testSuccessfulChecking() throws Exception {
+        assertTrue(periodFavoriteHandicap.check(
+                Who.HOME,
+                createPeriodHomeWonWithDifference(Math.abs(HANDICAP.intValue()) + 1)
+        ));
     }
 }
