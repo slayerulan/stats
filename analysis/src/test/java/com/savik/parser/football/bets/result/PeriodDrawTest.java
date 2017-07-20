8a02e00726e32e308c74675c43aa64999aea06b4
savik <savik94mail@gmail.com>
2017-07-20 12:50:52 +0300

    implemented PeriodDrawTest

---
+++ analysis/src/test/java/com/savik/parser/football/bets/result/PeriodDrawTest.java
@@ -0,0 +1,51 @@
+package com.savik.parser.football.bets.result;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import com.savik.football.bets.result.PeriodDraw;
+import com.savik.parser.football.bets.AbstractBetTest;
+import org.junit.Test;
+
+public class PeriodDrawTest extends AbstractBetTest {
+
+    @Test
+    public void shouldReturnTrueIfPeriodResultIsDraw() {
+        PeriodDraw periodDraw = new PeriodDraw();
+        assertTrue(periodDraw.check(
+                createPeriodHomeWonWithDifference(0)
+        ));
+    }
+
+    @Test
+    public void shouldReturnFalseIfPeriodResultIsNotDraw() {
+        PeriodDraw periodDraw = new PeriodDraw();
+        assertFalse(periodDraw.check(
+                createPeriodHomeWonWithDifference(1)
+        ));
+    }
+
+    @Test
+    public void shouldCanAnalyzeReturnTrueIfHomeAndGuestScoreExist() {
+        PeriodDraw periodDraw = new PeriodDraw();
+        assertTrue(periodDraw.canAnalyze(
+                createPeriodWithAnyGuestAndHomeScore()
+        ));
+    }
+
+    @Test
+    public void shouldCanAnalyzeReturnFalseIfHomeScoreNotExists() {
+        PeriodDraw periodDraw = new PeriodDraw();
+        assertFalse(periodDraw.canAnalyze(
+                createPeriodWithoutHomeScore()
+        ));
+    }
+
+    @Test
+    public void shouldCanAnalyzeReturnFalseIfGuestScoreNotExists() {
+        PeriodDraw periodDraw = new PeriodDraw();
+        assertFalse(periodDraw.canAnalyze(
+                createPeriodWithoutGuestScore()
+        ));
+    }
+}
