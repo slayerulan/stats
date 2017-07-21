f52080b42bb5aa1e7c6a425fa3a3a4e3ff5ba3ed
savik <savik94mail@gmail.com>
2017-07-21 14:06:02 +0300

    implemented PeriodFavoriteUnderTest

---
+++ analysis/src/test/java/com/savik/parser/football/bets/total/under/PeriodFavoriteUnderTest.java
@@ -0,0 +1,51 @@
+package com.savik.parser.football.bets.total.under;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import com.savik.football.bets.total.under.PeriodFavoriteUnder;
+import com.savik.football.model.Who;
+import com.savik.parser.football.bets.AbstractBetTest;
+import org.junit.Test;
+
+public class PeriodFavoriteUnderTest extends AbstractBetTest {
+
+    private static final Double TOTAL_LESS = 1.5;
+
+    @Test
+    public void shouldReturnTrueIfFavoriteScoredLess2() {
+        PeriodFavoriteUnder periodFavoriteUnder = new PeriodFavoriteUnder(TOTAL_LESS);
+        assertTrue(periodFavoriteUnder.check(
+                Who.HOME,
+                createPeriodWithHomeScore(1)
+        ));
+    }
+
+    @Test
+    public void shouldReturnFalseIfFavoriteScoredMore1() {
+        PeriodFavoriteUnder periodFavoriteUnder = new PeriodFavoriteUnder(TOTAL_LESS);
+        assertFalse(periodFavoriteUnder.check(
+                Who.HOME,
+                createPeriodWithHomeScore(2)
+        ));
+    }
+    
+
+    @Test
+    public void shouldCanAnalyzeReturnTrueIfFavoriteScoreExists() {
+        PeriodFavoriteUnder periodFavoriteUnder = new PeriodFavoriteUnder(TOTAL_LESS);
+        assertTrue(periodFavoriteUnder.canAnalyze(
+                Who.HOME,
+                createPeriodWithoutGuestScore()
+        ));
+    }
+
+    @Test
+    public void shouldCanAnalyzeReturnFalseIfFavoriteScoreNotExists() {
+        PeriodFavoriteUnder periodFavoriteUnder = new PeriodFavoriteUnder(TOTAL_LESS);
+        assertFalse(periodFavoriteUnder.canAnalyze(
+                Who.HOME,
+                createPeriodWithoutHomeScore()
+        ));
+    }
+}
