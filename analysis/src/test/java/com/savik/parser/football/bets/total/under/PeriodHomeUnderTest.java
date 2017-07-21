cfdbf5024cb0c109d03b11fd82ed24e449bad86a
savik <savik94mail@gmail.com>
2017-07-21 14:02:45 +0300

    implemented PeriodHomeUnderTest

---
+++ analysis/src/test/java/com/savik/parser/football/bets/total/under/PeriodHomeUnderTest.java
@@ -0,0 +1,45 @@
+package com.savik.parser.football.bets.total.under;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import com.savik.football.bets.total.under.PeriodHomeUnder;
+import com.savik.parser.football.bets.AbstractBetTest;
+import org.junit.Test;
+
+public class PeriodHomeUnderTest extends AbstractBetTest {
+
+    private static final Double TOTAL_LESS = 1.5;
+
+    @Test
+    public void shouldReturnTrueIfHomeScoredLess2() {
+        PeriodHomeUnder periodHomeUnder = new PeriodHomeUnder(TOTAL_LESS);
+        assertTrue(periodHomeUnder.check(
+                createPeriodWithHomeScore(1)
+        ));
+    }
+
+    @Test
+    public void shouldReturnFalseIfHomeScoredMore1() {
+        PeriodHomeUnder periodHomeUnder = new PeriodHomeUnder(TOTAL_LESS);
+        assertFalse(periodHomeUnder.check(
+                createPeriodWithHomeScore(2)
+        ));
+    }
+
+    @Test
+    public void shouldCanAnalyzeReturnTrueIfHomeScoreExists() {
+        PeriodHomeUnder periodHomeUnder = new PeriodHomeUnder(TOTAL_LESS);
+        assertTrue(periodHomeUnder.canAnalyze(
+                createPeriodWhereOnlyHomeScored()
+        ));
+    }
+
+    @Test
+    public void shouldCanAnalyzeReturnFalseIfHomeScoreNotExists() {
+        PeriodHomeUnder periodHomeUnder = new PeriodHomeUnder(TOTAL_LESS);
+        assertFalse(periodHomeUnder.canAnalyze(
+                createPeriodWithoutHomeScore()
+        ));
+    }
+}
