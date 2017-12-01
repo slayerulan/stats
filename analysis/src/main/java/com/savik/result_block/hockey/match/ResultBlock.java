7888de73fc1beb9b6135a72f1b14615227759adb
savik <savik94mail@gmail.com>
2017-12-01 14:00:28 +0300

    removed unused blocks

--- analysis/src/main/java/com/savik/result_block/hockey/match/ResultBlock.java
+++ analysis/src/main/java/com/savik/result_block/hockey/match/ResultBlock.java
@@ -4,8 +4,6 @@ import com.savik.ContainerType;
 import com.savik.GeneralBetContainer;
 import com.savik.MatchData;
 import com.savik.result_block.hockey.match.general.GeneralBlock;
-import com.savik.result_block.hockey.match.guest.GuestBlock;
-import com.savik.result_block.hockey.match.home.HomeBlock;
 
 import java.util.Arrays;
 
@@ -15,9 +13,7 @@ public class ResultBlock extends GeneralBetContainer {
     public ResultBlock(MatchData matchData) {
         super(
                 Arrays.asList(
-                        new GeneralBlock(matchData),
-                        new HomeBlock(),
-                        new GuestBlock()
+                        new GeneralBlock(matchData)
                 ), ContainerType.ROOT
         );
     }
