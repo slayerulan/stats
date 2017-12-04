b72e0cfe959f053f3150a342bf7d2b32ff06f512
savik <savik94mail@gmail.com>
2017-12-04 17:15:13 +0300

    added xls

--- query/src/test/java/com/savik/HockeyMatchParserTest.java
+++ query/src/test/java/com/savik/HockeyMatchParserTest.java
@@ -3,16 +3,20 @@ package com.savik;
 import com.savik.hockey.model.*;
 import com.savik.hockey.repository.HockeyMatchRepository;
 import com.savik.hockey.repository.HockeyTeamRepository;
+import com.savik.result_block.hockey.match.general.GeneralBlock;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
+import static com.savik.hockey.specifications.HockeyMatchSpec.hasTeam;
 import static org.junit.Assert.*;
 
 /**
@@ -22,6 +26,8 @@ import static org.junit.Assert.*;
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = Application.class)
 @ActiveProfiles("test")
+@Transactional
+@Rollback
 @Sql(scripts = {
         "classpath:db/matches_AqombNzc.sql", "classpath:db/matches_CIGVL5B6.sql",
         "classpath:db/matches_j5qIp46c.sql", "classpath:db/matches_rwPEPmCs.sql",
@@ -29,8 +35,11 @@ import static org.junit.Assert.*;
         "classpath:db/matches_vJ50rp5f.sql", "classpath:db/matches_YVeCUxb9.sql",
         "classpath:db/matches_I39zqxpU.sql", "classpath:db/matches_lAtkXvoI.sql",
         "classpath:db/matches_xn1B4zMd.sql", "classpath:db/matches_YaFolFyG.sql",
+        "classpath:db/matches_WAmcZ9Al.sql"
 })
 public class HockeyMatchParserTest {
+
+    public static final long COLORADO_TEAM_ID = 1L;
 
     @Autowired
     HockeyTeamRepository hockeyTeamRepository;
@@ -39,12 +48,25 @@ public class HockeyMatchParserTest {
     HockeyMatchRepository hockeyMatchRepository;
 
     @Test
-    public void testMatchWithOvertimeAndBulletsAndStats() {
+    public void testSize() {
+        List<HockeyMatch> matches = hockeyMatchRepository.findAll();
+        assertEquals(13, matches.size());
+    }
 
-        List<HockeyMatch> all = hockeyMatchRepository.findAll();
+
+    @Test
+    public void testColoradoStats() {
+        List<HockeyMatch> matches = hockeyMatchRepository.findAll(
+                hasTeam(COLORADO_TEAM_ID)
+        );
+        assertEquals(12, matches.size());
 
 
-        assertEquals(1, 1);
+        HockeyTeam team = hockeyTeamRepository.findOne(COLORADO_TEAM_ID);
+        MatchData matchData = new MatchData(team);
+
+        GeneralBlock generalBlock = new GeneralBlock(ContainerType.GENERAL, matchData);
+        matches.forEach(generalBlock::check);
     }
 
 
