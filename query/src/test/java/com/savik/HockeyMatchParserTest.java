c9fc1d1e3db92ef5d582c6fa4871a79a2d486c8a
savik <savik94mail@gmail.com>
2017-12-04 13:29:12 +0300

    added first 4 matches for general test

---
+++ query/src/test/java/com/savik/HockeyMatchParserTest.java
@@ -0,0 +1,46 @@
+package com.savik;
+
+import com.savik.hockey.model.*;
+import com.savik.hockey.repository.HockeyMatchRepository;
+import com.savik.hockey.repository.HockeyTeamRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Savushkin Yauheni
+ * @since 05.05.2017
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = Application.class)
+@ActiveProfiles("test")
+@Sql(scripts = {"classpath:db/matches_AqombNzc.sql", "classpath:db/matches_CIGVL5B6.sql",
+        "classpath:db/matches_j5qIp46c.sql", "classpath:db/matches_rwPEPmCs.sql"})
+public class HockeyMatchParserTest {
+
+    @Autowired
+    HockeyTeamRepository hockeyTeamRepository;
+
+    @Autowired
+    HockeyMatchRepository hockeyMatchRepository;
+
+    @Test
+    public void testMatchWithOvertimeAndBulletsAndStats() {
+
+        List<HockeyTeam> all = hockeyTeamRepository.findAll();
+
+
+        assertEquals(1, 1);
+    }
+
+
+
+}
