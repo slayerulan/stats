d0dff9f23f8992ae6fc083a21027a403e72b68e6
savik <savik94mail@gmail.com>
2017-12-12 12:47:35 +0300

    refactoring

--- predictions/src/main/java/com/savik/CoefficientsAnalyzer.java
+++ predictions/src/main/java/com/savik/CoefficientsAnalyzer.java
@@ -2,80 +2,118 @@ package com.savik;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class CoefficientsAnalyzer {
 
+    public static double LOW_VALUE_BORDER = 1.1;
+
     public static PossibleBetResultContainer analyze(CoeffContainer coeffContainer, PossibleBetContainer betContainer) {
         if (coeffContainer.getLeaf() && betContainer.getLeaf()) {
-            PossibleBet possibleBet = betContainer.getPossibleBet();
-            Coeff coeff = coeffContainer.getCoeff();
-
-            PossibleBetResultContainer possibleBetResultContainer = new PossibleBetResultContainer(betContainer.getType());
-            possibleBetResultContainer.setFirstTeamContainer(possibleBet.getFirstTeamContainer());
-            possibleBetResultContainer.setSecondTeamContainer(possibleBet.getSecondTeamContainer());
-            possibleBetResultContainer.setCoeff(coeff);
-
-
-            Integer firstTeamContainerPercentage = possibleBet.getFirstTeamPercentage();
-            Integer secondTeamContainerPercentage = possibleBet.getSecondTeamPercentage();
-            double averagePercentage = (double) (firstTeamContainerPercentage + secondTeamContainerPercentage) / 200;
-
-            possibleBetResultContainer.setPossibleBetStatus(PossibleBetStatus.SO_SO);
-            if (coeff.getStatus() == CoeffType.SINGLE) {
-                Double value = coeff.getValue();
-                double result = value * averagePercentage;
-                possibleBetResultContainer.setValueBet(result);
-                if (result > 1) {
-                    possibleBetResultContainer.setPossibleBetStatus(PossibleBetStatus.GOOD);
-                    possibleBetResultContainer.setCoeffType(CoeffType.SINGLE);
-                }
-            } else if (coeff.getStatus() == CoeffType.POSITIVE_NEGATIVE) {
-                Double positiveValue = coeff.getPositiveValue();
-                double positiveResult = positiveValue * averagePercentage;
-
-                Double negativeValue = coeff.getNegativeValue();
-                double negativeResult = negativeValue * (1 - averagePercentage);
-
-                if(positiveResult > 1 || negativeResult > 1) {
-                    if(positiveResult > negativeResult) {
-                        possibleBetResultContainer.setPossibleBetStatus(PossibleBetStatus.GOOD);
-                        possibleBetResultContainer.setCoeffType(CoeffType.POSITIVE);
-                        possibleBetResultContainer.setValueBet(positiveResult);
-                    } else {
-                        possibleBetResultContainer.setPossibleBetStatus(PossibleBetStatus.GOOD);
-                        possibleBetResultContainer.setCoeffType(CoeffType.NEGATIVE);
-                        possibleBetResultContainer.setValueBet(negativeResult);
-                    }
-                }
-            }
-
-            return possibleBetResultContainer;
-
+            return handleLeaf(coeffContainer, betContainer);
         } else {
-            List<? extends CoeffContainer> coeffContainerChildrenBlocks = coeffContainer.getChildrenBlocks();
-            List<PossibleBetContainer> possibleBetContainerBlocks = betContainer.getChildrenBetBlocks();
-            // TODO: remove
+            return handleContainer(coeffContainer, betContainer);
+
+        }
+    }
+
+    private static PossibleBetResultContainer handleLeaf(CoeffContainer coeffContainer, PossibleBetContainer betContainer) {
+        PossibleBet possibleBet = betContainer.getPossibleBet();
+        Coeff coeff = coeffContainer.getCoeff();
+
+        PossibleBetResultContainer possibleBetResultContainer = new PossibleBetResultContainer(betContainer.getType());
+        possibleBetResultContainer.setFirstTeamContainer(possibleBet.getFirstTeamContainer());
+        possibleBetResultContainer.setSecondTeamContainer(possibleBet.getSecondTeamContainer());
+        possibleBetResultContainer.setCoeff(coeff);
+
+
+        Double firstTeamContainerPercentage = (double) possibleBet.getFirstTeamPercentage() / 100;
+        Double secondTeamContainerPercentage = (double) possibleBet.getSecondTeamPercentage() / 100;
+
+        possibleBetResultContainer.setPossibleBetStatus(PossibleBetStatus.SO_SO);
+        if (coeff.getStatus() == CoeffType.SINGLE) {
+            handleSingleCoeff(
+                    coeff, possibleBetResultContainer, firstTeamContainerPercentage, secondTeamContainerPercentage);
+        } else if (coeff.getStatus() == CoeffType.POSITIVE_NEGATIVE) {
+            handlePositiveNegativeCoeff(
+                    coeff, possibleBetResultContainer,
+                    firstTeamContainerPercentage, secondTeamContainerPercentage
+            );
+        } else {
+            throw new RuntimeException("coeff status is weird: " + coeff.getStatus());
+        }
+
+        return possibleBetResultContainer;
+    }
+
+    private static void handlePositiveNegativeCoeff(Coeff coeff, PossibleBetResultContainer possibleBetResultContainer,
+                                                    Double firstTeamContainerPercentage, Double secondTeamContainerPercentage) {
+        Double positiveValue = coeff.getPositiveValue();
+        double firstTeamPositiveResult = positiveValue * firstTeamContainerPercentage;
+        double secondTeamPositiveResult = positiveValue * secondTeamContainerPercentage;
+
+
+        Double negativeValue = coeff.getNegativeValue();
+        double firstTeamNegativeResult = negativeValue * (1 - firstTeamContainerPercentage);
+        double secondTeamNegativeResult = negativeValue * (1 - secondTeamContainerPercentage);
+
+        double[] values = {firstTeamPositiveResult, secondTeamPositiveResult,
+                firstTeamNegativeResult, secondTeamNegativeResult};
+
+        double maxValue = Arrays.stream(values).max().getAsDouble();
+
+        possibleBetResultContainer.setValueBet(maxValue);
+        if (maxValue > LOW_VALUE_BORDER) {
+            possibleBetResultContainer.setPossibleBetStatus(PossibleBetStatus.GOOD);
+        }
+
+        if (maxValue == firstTeamPositiveResult || maxValue == secondTeamPositiveResult) {
+            possibleBetResultContainer.setCoeffType(CoeffType.POSITIVE);
+        } else {
+            possibleBetResultContainer.setCoeffType(CoeffType.NEGATIVE);
+        }
+    }
+
+    private static void handleSingleCoeff(Coeff coeff, PossibleBetResultContainer possibleBetResultContainer, Double firstTeamContainerPercentage, Double secondTeamContainerPercentage) {
+        possibleBetResultContainer.setCoeffType(CoeffType.SINGLE);
+
+        Double value = coeff.getValue();
+        double firstTeamResult = value * firstTeamContainerPercentage;
+        double secondTeamResult = value * secondTeamContainerPercentage;
+        if (firstTeamResult > secondTeamResult) {
+            possibleBetResultContainer.setValueBet(firstTeamResult);
+        } else {
+            possibleBetResultContainer.setValueBet(secondTeamResult);
+        }
+
+        if (firstTeamResult > LOW_VALUE_BORDER || secondTeamResult > LOW_VALUE_BORDER) {
+            possibleBetResultContainer.setPossibleBetStatus(PossibleBetStatus.GOOD);
+        }
+    }
+
+    private static PossibleBetResultContainer handleContainer(CoeffContainer coeffContainer, PossibleBetContainer betContainer) {
+        List<? extends CoeffContainer> coeffContainerChildrenBlocks = coeffContainer.getChildrenBlocks();
+        List<PossibleBetContainer> possibleBetContainerBlocks = betContainer.getChildrenBetBlocks();
+        // TODO: remove
             /*if (coeffContainerChildrenBlocks.size() != possibleBetContainerBlocks.size()) {
                 throw new IllegalArgumentException("coeffContainer is incompatible with betContainer");
             }*/
-            List<PossibleBetResultContainer> childBlocks = new ArrayList<>();
-            for (int i = 0; i < coeffContainerChildrenBlocks.size(); i++) {
-                PossibleBetContainer childPossibleBetContainer = possibleBetContainerBlocks.get(i);
-                CoeffContainer childCoeffContainer = coeffContainerChildrenBlocks.get(i);
-                if (childPossibleBetContainer.getType() != childCoeffContainer.getType()) {
-                    throw new IllegalArgumentException("childPossibleBetContainer type = " + childPossibleBetContainer.getType() + "," +
-                            " childCoeffContainer type = " + childCoeffContainer.getType());
-                }
-                PossibleBetResultContainer childResult = analyze(childCoeffContainer, childPossibleBetContainer);
-                childBlocks.add(childResult);
+        List<PossibleBetResultContainer> childBlocks = new ArrayList<>();
+        for (int i = 0; i < coeffContainerChildrenBlocks.size(); i++) {
+            PossibleBetContainer childPossibleBetContainer = possibleBetContainerBlocks.get(i);
+            CoeffContainer childCoeffContainer = coeffContainerChildrenBlocks.get(i);
+            if (childPossibleBetContainer.getType() != childCoeffContainer.getType()) {
+                throw new IllegalArgumentException("childPossibleBetContainer type = " + childPossibleBetContainer.getType() + "," +
+                        " childCoeffContainer type = " + childCoeffContainer.getType());
             }
-
-            return new PossibleBetResultContainer(
-                    childBlocks,
-                    betContainer.getType()
-            );
-
+            PossibleBetResultContainer childResult = analyze(childCoeffContainer, childPossibleBetContainer);
+            childBlocks.add(childResult);
         }
+
+        return new PossibleBetResultContainer(
+                childBlocks,
+                betContainer.getType()
+        );
     }
 }
